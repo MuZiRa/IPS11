a2c66b509da65f1e31352f876a6f9fd537cc1cc4
batrisyiasafri <20FTT1444@student.pb.edu.bn>
2022-08-05 21:21:08 +0800

    Added Admin Sellers Page

---
+++ src/app/admin/sellers/sellers.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sellers',
+  templateUrl: './sellers.page.html',
+  styleUrls: ['./sellers.page.scss'],
+})
+export class SellersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
