26e0407d724522078303226a7119e4e6b496d2f7
batrisyiasafri <20FTT1444@student.pb.edu.bn>
2022-08-05 20:07:50 +0800

    Updated Navigation In Admin Page

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -7,7 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+      //when navigate to dashboard page
+      toDash(){
+
+        this.router.navigateByUrl('/admin/dashboard');
+      }
+    
+      //when navigate to product page
+      toProduct(){
+
+        this.router.navigateByUrl('/admin/product');
+      }
+  
+    //when navigate to sellers page
+      toSeller(){
+
+        this.router.navigateByUrl('/admin/sellers');
+      }
+  
+     //when navigate to orders page
+     toOrder(){
+      this.router.navigateByUrl('/admin/orders');
+    }
+  
+     //when navigate to shipments page
+     toShip(){
+      this.router.navigateByUrl('/admin/shipments');
+    }
+
+     //when navigate to reviews page
+      toReviews(){
+      this.router.navigateByUrl('/admin/reviews');
+    }
+
+     //when navigate to invoice page
+     toInovice(){
+      this.router.navigateByUrl('/invoice');
+    }
+    
 
   ngOnInit() {
   }
