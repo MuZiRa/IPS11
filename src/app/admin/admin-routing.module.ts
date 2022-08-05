8982617ff9dcd62b1a95ce4a4c966543a84151c3
batrisyiasafri <20FTT1444@student.pb.edu.bn>
2022-08-05 21:20:54 +0800

    Updated Admin Routing Page

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -11,6 +11,30 @@ const routes: Routes = [
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'orders',
+    loadChildren: () => import('./orders/orders.module').then( m => m.OrdersPageModule)
+  },
+  {
+    path: 'sellers',
+    loadChildren: () => import('./sellers/sellers.module').then( m => m.SellersPageModule)
+  },
+  {
+    path: 'product',
+    loadChildren: () => import('./product/product.module').then( m => m.ProductPageModule)
+  },
+  {
+    path: 'reviews',
+    loadChildren: () => import('./reviews/reviews.module').then( m => m.ReviewsPageModule)
+  },
+  {
+    path: 'shipments',
+    loadChildren: () => import('./shipments/shipments.module').then( m => m.ShipmentsPageModule)
+  },
+  {
+    path: 'invoice',
+    loadChildren: () => import('./invoice/invoice.module').then( m => m.InvoicePageModule)
   }
 ];
 
