a88826be103c45338984022957fbdbbe9379ec75
batrisyiasafri <20FTT1444@student.pb.edu.bn>
2022-08-06 14:04:39 +0800

    Updated Admin Sellers Page

--- src/app/admin/sellers/sellers.page.ts
+++ src/app/admin/sellers/sellers.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sellers',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellersPage implements OnInit {
 
-  constructor() { }
+    //mock data for seller
+    sellers: any = [
+      {
+        "username": "Noor Aiman",
+        "potrait": "assets/potraits/pro2.png",
+        "company": "EcoThrifty Company"
+      },
+      {
+        "username": "Farah Amani",
+        "potrait": "assets/potraits/pro5.png",
+        "company": "DeThrift Company"
+      },
+      {
+        "username": "Siti Ropeah",
+        "potrait": "assets/potraits/pro4.png",
+        "company": "KlothThrift Company"
+      },
+      {
+        "username": "Abdul Aziz",
+        "potrait": "assets/potraits/pro3.png",
+        "company": "LocalThrift Company"
+      },
+      {
+        "username": "Ahmad Azim",
+        "potrait": "assets/potraits/pro7.png",
+        "company": "ThriftTah Company"
+      },
+    ];
+
+  listOfSellers: any;
+
+  constructor(public navCtrl: NavController) {
+              this.listOfSellers = this.sellers;
+   }
+
+      //to filter sellers by username 
+      searchSeller(ev: any) {
+        let val = ev.target.value;
+      
+        if (val && val.trim() !== '') {
+          this.listOfSellers = this.sellers.filter((seller) => {
+            return seller.username.toLowerCase().includes(val.toLowerCase());
+          });
+        } else {
+          this.listOfSellers = this.sellers;
+        }
+        }
 
   ngOnInit() {
   }
+
+
 
 }
