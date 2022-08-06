e9b19e0156603fd0b99cb7fbc6d8c4c3ad0ae3a1
batrisyiasafri <20FTT1444@student.pb.edu.bn>
2022-08-06 14:51:26 +0800

    Added product.model.ts

---
+++ src/app/admin/product/product.model.ts
@@ -0,0 +1,12 @@
+export class Product{
+    constructor(
+        public id: string,
+        public produtId: string,
+        public sellerId: string,
+        public productTitle: string,
+        public productImage: string,
+        public company: string,
+        public description: string,
+
+    ){} 
+}
